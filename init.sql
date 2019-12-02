def071e76d071fa0d6064fb7b7a07e65b3a5bdf2
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-12-02 17:31:24 +0100

    Create the table for categories

--- init.sql
+++ init.sql
@@ -3,3 +3,14 @@ DROP DATABASE IF EXISTS PBS;
 CREATE DATABASE PBS CHARACTER SET 'utf8mb4';
 
 USE PBS;
+
+-- ---------------------------------------
+-- CREATE A TABLE FOR Categories
+-- ---------------------------------------
+
+CREATE TABLE Categories (
+    id INT UNSIGNED AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    url VARCHAR(250) NOT NULL,
+    PRIMARY KEY(id)
+)

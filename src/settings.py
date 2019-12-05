8ae7b203a0ae19e19de32f62d7f6f4cce17fdc3c
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-12-05 12:26:35 +0100

    Add settings for using the API

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,20 @@
     Settings file for PurBeurreSubstitute
 """
 
+# MySQL informations
 PBS_USER = 'pbs'
 PBS_HOST = 'localhost'
 PBS_PASSWORD = 'pbs'
+
+# Temp dir in which we download data
+TMP_DIR = 'tmp/'
+
+# Parameters for download data
+API_PAGE_SIZE = 100
+API_PAGES = 10
+API_URL_BASE = 'https://fr.openfoodfacts.org/cgi/search.pl'
+API_CATEGORIES = ['aliments-d-origine-vegetale',
+                  'viandes',
+                  'boissons',
+                  'confiseries',
+                  'produits-laitiers']

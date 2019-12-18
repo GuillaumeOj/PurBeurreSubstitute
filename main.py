b8c206b9dd7d21eb7f6bbd6c3e12b2a5e1f8e17a
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-12-18 12:39:30 +0100

    Create 'init_pur_beurre' for clean the main function

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
         - a database like MySQL,
         - and of course Python.
 """
-from settings import * # pylint: disable=wildcard-import
+from init import init_pur_beurre
+from settings import PBS_DB_NAME, PBS_USER, PBS_HOST, PBS_PASSWORD
 from src.database import Database
-from src.api import Api
-from src.product import Product
+# from src.product import Product
 from src.interface import SelectionMenu
 
 
@@ -17,6 +17,7 @@ def main():
     """
         Main function for running the application
     """
+    print('=== Bienvenue dans l\'applcation Pur Beurre Substitute ===')
 
     # Connect to the database
     pbs_db = Database(PBS_DB_NAME, PBS_USER, PBS_HOST, PBS_PASSWORD)
@@ -24,101 +25,7 @@ def main():
 
     # Check if database is not empty
     if pbs_db.check_database() is None:
-        # Initialize the API
-        api = Api(API_URL_BASE, TMP_DIR)
-        # Download data with the API
-        api.download_products(API_CATEGORIES, API_PAGE_SIZE, API_PAGES)
-
-        # Read data downloaded
-        api.read_json_with_key('products')
-
-        # Define each product as an object with variables attributes
-        for line in api.data:
-            Product(**line)
-
-        # Insert products in the database
-        for product in Product.products:
-
-            required_attributes = [product.categories,
-                                   product.brands,
-                                   product.stores,
-                                   product.id_ext,
-                                   product.name,
-                                   product.nova,
-                                   product.nutriscore,
-                                   product.url]
-            if all(attribute is not None for attribute in required_attributes):
-                # First insert categories
-                query = ('INSERT INTO Categories'
-                         '(name)'
-                         'VALUES (%s)')
-                for category in product.categories:
-                    values = (category,)
-                    pbs_db.insert_in_database(query, values)
-
-                # Second insert brands
-                query = ('INSERT INTO Brands'
-                         '(name)'
-                         'VALUES (%s)')
-                for brand in product.brands:
-                    values = (brand,)
-                    pbs_db.insert_in_database(query, values)
-
-                # Third insert stores
-                query = ('INSERT INTO Stores'
-                         '(name)'
-                         'VALUES (%s)')
-                for store in product.stores:
-                    values = (store,)
-                    pbs_db.insert_in_database(query, values)
-
-                # Then insert the product
-                query = ('INSERT INTO Products'
-                         '(id_ext, name, common_name, quantity, ingredients_list, nova_group,'
-                         'nutriscore_grade, url)'
-                         'VALUES (%s, %s, %s, %s, %s, %s, %s, %s)')
-                values = (product.id_ext,
-                          product.name,
-                          product.common_name,
-                          product.quantity,
-                          product.ingredients,
-                          product.nova,
-                          product.nutriscore,
-                          product.url)
-                pbs_db.insert_in_database(query, values)
-
-                # Insert products categories
-                query = ('INSERT INTO Products_categories'
-                         '(product_id, category_id)'
-                         'VALUES'
-                         '((SELECT id FROM Products WHERE name=%s),'
-                         '(SELECT id FROM Categories WHERE name=%s))')
-                for category in product.categories:
-                    values = (product.name, category)
-                    pbs_db.insert_in_database(query, values)
-
-                # Insert products brands
-                query = ('INSERT INTO Products_brands'
-                         '(product_id, brand_id)'
-                         'VALUES'
-                         '((SELECT id FROM Products WHERE name=%s),'
-                         '(SELECT id FROM Brands WHERE name=%s))')
-                for brand in product.brands:
-                    values = (product.name, brand)
-                    pbs_db.insert_in_database(query, values)
-
-                # Insert products stores
-                query = ('INSERT INTO Products_stores'
-                         '(product_id, store_id)'
-                         'VALUES'
-                         '((SELECT id FROM Products WHERE name=%s),'
-                         '(SELECT id FROM Stores WHERE name=%s))')
-                for store in product.stores:
-                    values = (product.name, store)
-                    pbs_db.insert_in_database(query, values)
-
-        # Delete temporary files
-        api.delete_files()
+        init_pur_beurre(pbs_db)
 
     # Here start the application
     while True:
@@ -132,7 +39,7 @@ def main():
         # Get the user answer for the choosen category
         categories = SelectionMenu(*choices)
         categories.display_choices('Choisissez une catégorie')
-        categories.user_input('Sélectionner un catégorie (numéro)')
+        categories.user_input('Sélectionne un catégorie (numéro)')
 
         # Display available products in the chossen category
         query = ("""SELECT Products.name FROM Products

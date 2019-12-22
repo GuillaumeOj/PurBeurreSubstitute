beb7ba35e773057a2ead63ea99958eea1aca7ac8
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-12-22 16:13:47 +0100

    Create Category

---
+++ src/category.py
@@ -0,0 +1,17 @@
+"""
+    This module define a category and all method associated
+"""
+
+
+class Category: # pylint: disable=too-many-instance-attributes
+    """
+        This class create a category
+    """
+
+    def __init__(self, name):
+
+        self.name = name
+
+
+if __name__ == '__main__':
+    print('Please don\'t load me alone...')

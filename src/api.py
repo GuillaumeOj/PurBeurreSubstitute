c651da2a1abf77d81f20626cdca72c50165fc9e6
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-12-27 17:00:40 +0100

    Fix API search problem

--- src/api.py
+++ src/api.py
@@ -52,15 +52,16 @@ class Api:
             headers = {'User-agent': 'Pur Beurre Substitute - Mac OS X 10.13 - Version 3.0'}
 
             # A progress bar for seeing the application working
-            progress_bar = FillingCirclesBar(f'Téléchargement en cours de la catégorie\
-                                             "{category}" :',
-                                             max=self.pages)
+            progress_bar = f'Téléchargement en cours de la catégorie "{category}" :'
+            progress_bar = FillingCirclesBar(progress_bar, max=self.pages)
             for page in range(self.pages):
                 # Parameters sent with te request
                 parameters = {'json': 1,
                               'page_size': self.page_size,
-                              'page': page,
-                              'categorie': category,
+                              'page': (page + 1),
+                              'tagtype_0': 'categories',
+                              'tag_contains_0': 'contains',
+                              'tag_0': category,
                               'action': 'process'}
 
                 # File in wich data are saved
@@ -86,6 +87,9 @@ class Api:
         """
             This method read json files and return only data on specific key
         """
+        progress_bar = 'Lecture des données en cours :'
+        progress_bar_count = len(self.categories) * self.pages
+        progress_bar = FillingCirclesBar(progress_bar, max=progress_bar_count)
         for category in self.categories:
             for page in range(self.pages):
                 # Create a path for the file
@@ -99,12 +103,16 @@ class Api:
                     # Store data in list
                     for line in json_data[key]:
                         self.data.append(line)
+                progress_bar.next()
+        progress_bar.finish()
 
     def keep_required(self, data_required):
         """
             This method drop data with missing keys
         """
         index_list = list()
+        progress_bar = 'Nettoyage des produits avec données manquantes :'
+        progress_bar = FillingCirclesBar(progress_bar, max=len(self.data))
         for i, dictionary in enumerate(self.data):
             try:
                 for required in data_required:
@@ -121,6 +129,8 @@ class Api:
             except KeyError:
                 # Save the data's index if there is a key error
                 index_list.append(i)
+            progress_bar.next()
+        progress_bar.finish()
 
         index_list.reverse()
 
@@ -132,6 +142,8 @@ class Api:
         """
             This method format the data to the required format for the database
         """
+        progress_bar = 'Formattage des données :'
+        progress_bar = FillingCirclesBar(progress_bar, max=len(self.data))
         for i, dictionary in enumerate(self.data):
             for key_format in data_format:
                 key = key_format['name']
@@ -148,6 +160,8 @@ class Api:
                         dictionary[key] = self.string_to_list(dictionary[key])
 
                 self.data[i] = dictionary
+            progress_bar.next()
+        progress_bar.finish()
 
     @staticmethod
     def string_to_list(string, separator=','):
